7c305504a7193f0d0a6f8caaa4d6ac6c3c13b154
Ari Purnama <ari.purnama@citra.digital>
2020-11-21 14:17:58 +0800

    new commit

---
+++ config.py
@@ -0,0 +1,2 @@
+class Config:
+    DB_URL = "postgres://ofqsmsmczbhjtw:d4cde078b5ecf40c01ad4a07d8cffe0d10937c83227b6ba01842b2f193abf93c@ec2-54-158-190-214.compute-1.amazonaws.com:5432/da9fdmsseqe1ea"
